b81af1fd6e6655bcb46393a3bf13e1b0f232f209
fenilhansaliami <fenil.hansalia@mindinventory.com>
2021-02-24 12:07:26 +0530

    plugins added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,90 @@ import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import MathType from "@wiris/mathtype-ckeditor5";
 import ExportPdf from "@ckeditor/ckeditor5-export-pdf/src/exportpdf";
 
+import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
+
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
+import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+import Image from "@ckeditor/ckeditor5-image/src/image";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import List from "@ckeditor/ckeditor5-list/src/list";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import Table from "@ckeditor/ckeditor5-table/src/table";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
+
 const editorConfiguration = {
-  plugins: [Essentials, Bold, Italic, Paragraph, MathType, ExportPdf],
-  toolbar: ["bold", "italic", "MathType", "ChemType", "exportPdf"],
+  plugins: [
+    Essentials,
+    Bold,
+    Italic,
+    Paragraph,
+    MathType,
+    ExportPdf,
+    UploadAdapter,
+    Autoformat,
+    BlockQuote,
+    CKFinder,
+    EasyImage,
+    Heading,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    Link,
+    List,
+    MediaEmbed,
+    PasteFromOffice,
+    Table,
+    TableToolbar,
+    TextTransformation,
+  ],
+  toolbar: {
+    items: [
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "link",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "indent",
+      "outdent",
+      "|",
+      "imageUpload",
+      "blockQuote",
+      "insertTable",
+      "mediaEmbed",
+      "undo",
+      "redo",
+      "MathType",
+      "ChemType",
+      "exportPdf",
+    ],
+  },
+  image: {
+    toolbar: [
+      "imageStyle:full",
+      "imageStyle:side",
+      "|",
+      "imageTextAlternative",
+    ],
+  },
+  table: {
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+  },
   exportPdf: {
     stylesheets: [
       "./path/to/fonts.css",
